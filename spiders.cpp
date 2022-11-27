508a5b3743bb37e82d24a56213a1452bc7014992
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2022-11-27 11:10:41 -0500

    spiders.cpp: Refactor to make Troll more object-like

--- spiders.cpp
+++ spiders.cpp
@@ -4,11 +4,12 @@
 #include <utility>
 #include <vector>
 
-struct Troll {
+template<class Derived>
+struct TrollBase {
     struct promise_type;
     using handle_t = std::coroutine_handle<promise_type>;
     struct promise_type {
-        auto get_return_object() { return Troll(handle_t::from_promise(*this)); }
+        Derived get_return_object() { Derived d; d.coro_ = handle_t::from_promise(*this); return d; }
         auto initial_suspend() { return std::suspend_always(); }
         auto final_suspend() noexcept { return std::suspend_never(); }
         auto yield_value(bool b) noexcept {
@@ -19,13 +20,10 @@ struct Troll {
         bool value_ = false;
     };
 
-    explicit Troll() = default;
-    Troll(Troll&& rhs) noexcept : coro_(std::exchange(rhs.coro_, nullptr)) {}
-    Troll& operator=(Troll rhs) noexcept {
-        std::swap(coro_, rhs.coro_);
-        return *this;
-    }
-    ~Troll() { if (coro_) coro_.destroy(); }
+    explicit TrollBase() = default;
+    TrollBase(TrollBase&& rhs) noexcept : coro_(std::exchange(rhs.coro_, nullptr)) {}
+    void operator=(TrollBase rhs) noexcept { std::swap(coro_, rhs.coro_); }
+    ~TrollBase() { if (coro_) coro_.destroy(); }
 
     bool poke() {
         coro_.resume(); // should modify value_
@@ -33,17 +31,13 @@ struct Troll {
     }
 
 private:
-    explicit Troll(handle_t h) : coro_(h) {}
-
     handle_t coro_ = nullptr;
 };
 
 void unconstrained(int n) {
     // SPCL page 550
-    auto lamps = std::deque<bool>(n, false);
-    auto trolls = std::vector<Troll>(n);
-    auto make_troll = [&](int k) {
-        return [](Troll *next_troll, bool *lamp) -> Troll {
+    struct UnconstrainedTroll : TrollBase<UnconstrainedTroll> {
+        static UnconstrainedTroll make(UnconstrainedTroll *next_troll, bool *lamp) {
             while (true) {
                 // We've been poked while awake!
                 *lamp = !*lamp;
@@ -51,10 +45,13 @@ void unconstrained(int n) {
                 // We've been poked -- awake!
                 co_yield (next_troll ? next_troll->poke() : false);
             }
-        }(k >= 1 ? &trolls[k-1] : nullptr, &lamps[k]);
+        }
     };
+
+    auto lamps = std::deque<bool>(n, false);
+    auto trolls = std::vector<UnconstrainedTroll>(n);
     for (int i=0; i < n; ++i) {
-        trolls[i] = make_troll(i);
+        trolls[i] = UnconstrainedTroll::make(i > 0 ? &trolls[i-1] : nullptr, &lamps[i]);
     }
     while (trolls[n-1].poke()) {
         printf("Lamps are: ");
@@ -67,15 +64,11 @@ void unconstrained(int n) {
 
 void chains(int n) {
     // SPCL page 552
-    auto lamps = std::deque<bool>(n, false);
-    auto trolls = std::vector<Troll>(n);
-    auto make_troll = [&](int k) {
-        return [](Troll *next_troll, bool *lamp) -> Troll {
+    struct ChainTroll : TrollBase<ChainTroll> {
+        static ChainTroll make(ChainTroll *next_troll, bool *lamp) {
             while (true) {
                 // awake0
-                if (next_troll) {
-                    while (next_troll->poke()) co_yield true;
-                }
+                while (next_troll && next_troll->poke()) co_yield true;
                 *lamp = 1;
                 co_yield true;
                 // asleep1
@@ -84,15 +77,15 @@ void chains(int n) {
                 *lamp = 0;
                 co_yield true;
                 // asleep0
-                if (next_troll) {
-                    while (next_troll->poke()) co_yield true;
-                }
+                while (next_troll && next_troll->poke()) co_yield true;
                 co_yield false;
             }
-        }(k >= 1 ? &trolls[k-1] : nullptr, &lamps[k]);
+        }
     };
+    auto lamps = std::deque<bool>(n, false);
+    auto trolls = std::vector<ChainTroll>(n);
     for (int i=0; i < n; ++i) {
-        trolls[i] = make_troll(i);
+        trolls[i] = ChainTroll::make(i > 0 ? &trolls[i-1] : nullptr, &lamps[i]);
     }
     while (trolls[n-1].poke() || lamps[n-1]) {
         printf("Lamps are: ");
@@ -105,12 +98,8 @@ void chains(int n) {
 
 void fence_digraph(int n) {
     // SPCL page 557
-    auto lamps = std::deque<bool>(n, false);
-    auto trolls = std::vector<Troll>(n);
-    auto make_troll = [&](int k) {
-        int kp = k + 1 + (k % 2);
-        int kpp = k + 2 - (k % 2);
-        return [](Troll *trollp, Troll *trollpp, bool *lamp) -> Troll {
+    struct FenceTroll : TrollBase<FenceTroll> {
+        static FenceTroll make(FenceTroll *trollp, FenceTroll *trollpp, bool *lamp) {
             if (*lamp) goto awake1;
             while (true) {
                 // awake0
@@ -128,11 +117,15 @@ void fence_digraph(int n) {
                 while (trollp && trollp->poke()) co_yield true;
                 co_yield false;
             }
-        }(kp < n ? &trolls[kp] : nullptr, kpp < n ? &trolls[kpp] : nullptr, &lamps[k]);
+        }
     };
+    auto lamps = std::deque<bool>(n, false);
+    auto trolls = std::vector<FenceTroll>(n);
     for (int i=0; i < n; ++i) {
+        int kp = i + 1 + (i % 2);
+        int kpp = i + 2 - (i % 2);
         lamps[i] = (i / 3) % 2;
-        trolls[i] = make_troll(i);
+        trolls[i] = FenceTroll::make(kp < n ? &trolls[kp] : nullptr, kpp < n ? &trolls[kpp] : nullptr, &lamps[i]);
     }
     while (trolls[0].poke()) {
         printf("Lamps are: ");
